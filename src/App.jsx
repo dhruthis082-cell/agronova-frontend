1022aa2c9b55a49815c07ac26b6a79f745d6af15
Dhruthi <dhruthi082@gmail.com>
2026-05-17 13:38:33 +0530

    fix build error

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 
-import Cart from "./pages/Cart";
-
-import Payment from "./pages/Payment";
-
 function App() {
   return (
     <BrowserRouter>
-
       <Routes>
-
         <Route path="/" element={<Home />} />
-
-        <Route path="/cart" element={<Cart />} />
-
-        <Route path="/payment" element={<Payment />} />
-
       </Routes>
-
     </BrowserRouter>
   );
 }
